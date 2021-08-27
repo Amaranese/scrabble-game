dc7e8392796974ca11540643a92b5bbf57b365a1
Amaranese <alejandro.maranes@asendia.com>
2021-08-27 15:31:59 +0200

    Create Word.cs

---
+++ Scrabble/Core/Words/Word.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrabble.Core.Words
+{
+    public class Word
+    {
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        public int EndX { get; set; }
+        public int EndY { get; set; }
+        public int Score { get; set; }
+        public string Text { get; set; }
+        public List<ScrabbleTile> Tiles { get; set; }
+        public bool Valid { get; set; }
+
+        public void SetValidHighlight()
+        {
+            foreach (var t in Tiles)
+                t.OnHighlight(Valid);
+        }
+
+        public void ClearValidHighlight()
+        {
+            foreach (var t in Tiles)
+                t.ClearHighlight();
+        }
+
+        public override string ToString()
+        {
+            return $"<Word Text={Text}, SX={StartX}, EX={EndX}, SY={StartY}, EY={EndY}, Score={Score} />";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Word)
+            {
+                var word = (Word) obj;
+                return word.Text == this.Text && word.StartX == this.StartX && word.StartY == this.StartY && word.EndX == this.EndX && word.EndY == this.EndY;
+            }
+
+            return false;
+        }
+    }
+}
