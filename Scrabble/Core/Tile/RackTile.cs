03f2615cf49afdd31a5a9e79766d106795d49aa9
Amaranese <alejandro.maranes@asendia.com>
2021-08-27 15:32:12 +0200

    Create RackTile.cs

---
+++ Scrabble/Core/Tile/RackTile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrabble.Core
+{
+    public class RackTile : Button
+    {
+        public char Letter { get; set; }
+        public int LetterValue { get; set; }
+        public bool LetterSelected { get; set; }
+
+        public void ClearDisplay()
+        {
+            this.LetterSelected = false;
+            this.FlatStyle = FlatStyle.Standard;
+            this.Text = string.Empty;
+        }
+
+        public void OnLetterSelected()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderColor = Color.LimeGreen;
+            this.FlatAppearance.BorderSize = 5;
+            this.LetterSelected = true;
+        }
+
+        public void OnLetterDeselected()
+        {
+            this.FlatStyle = FlatStyle.Standard;
+            this.LetterSelected = false;
+        }
+    }
+}
